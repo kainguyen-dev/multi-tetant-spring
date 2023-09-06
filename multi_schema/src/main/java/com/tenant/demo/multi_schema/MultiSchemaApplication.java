72212ef5fddc0f464dc46b09eebc9194f585a670
Kai_Nguyen <duy.nguyen@diabolocom.com>
2023-09-06 18:53:42 +0700

    Init modules

---
+++ multi_schema/src/main/java/com/tenant/demo/multi_schema/MultiSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.tenant.demo.multi_schema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSchemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiSchemaApplication.class, args);
+    }
+
+}
